98bf7954635fa4acffb53f74686e02fceb6b645c
bitores <773155801@qq.com>
2016-04-27 23:58:54 +0800

    ffd

--- bijia-demo/src/js/router2.js
+++ bijia-demo/src/js/router2.js
@@ -9,15 +9,43 @@ define(['angular','angular-ui-router','angular-resource','angular-animate','angu
             console.log('app run ...');
             //方便获得当前状态的方法，绑到根作用域
             $rootScope.$state = $state;
-            $rootScope.$stateParams = $stateParams
+            $rootScope.$stateParams = $stateParams;
+
+            console.log($state,$stateParams);
+
+
+            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){console.log('stateChangeStart');})
+            $rootScope.$on('$stateNotFound', function(event, unfoundState, fromState, fromParams){console.log('stateNotFound');})
+            $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){console.log('stateChangeSuccess');})
+            $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error){console.log('stateChangeError');})
+
+            $rootScope.$on('$viewContentLoading', function(event, viewConfig){console.log('viewContentLoading',viewConfig);})
+            $rootScope.$on('$viewContentLoaded', function(event){console.log('viewContentLoaded');})
         }
      ])
-    app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $uiViewScrollProvider){
+    app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $uiViewScrollProvider){//,$rootScope
         console.log('route config ...');
         //用于改变state时跳至顶部
         // $uiViewScrollProvider.useAnchorScroll();
         // 默认进入先重定向
-        // $urlRouterProvider.when("", "/PageTab");
+        // $urlRouterProvider
+        //     .when("", {  
+        //         templateUrl:"/src/views/PageTab.html",
+        //         controller:function(){}  
+        //     }).when("/PageTab", {  
+        //         templateUrl:"/src/views/PageTab.html",
+        //         controller:function(){}  
+        //     })
+            // .when("PageTab/Page1", {  
+            //     templateUrl:"/src/views/Page1.html",
+            //     controller:function(){}  
+            // }).when("PageTab/Page2", {  
+            //     templateUrl:"/src/views/Page2.html",
+            //     controller:function(){}  
+            // }).when("PageTab/Page3", {  
+            //     templateUrl:"/src/views/Page3.html",
+            //     controller:function(){}  
+            // });
         // $urlRouterProvider.otherwise('/');
         $stateProvider
             // .state({
@@ -57,12 +85,15 @@ define(['angular','angular-ui-router','angular-resource','angular-animate','angu
 
                 }
             });
-    $urlRouterProvider.otherwise('/PageTab');
+            $urlRouterProvider.otherwise('/PageTab');
+
+
 
         //全局添加API接口指标
         // $httpProvider.defaults.headers.common["Accept"] = "version=1.0.1&client_type=wap";
         //开启HTML5链接 
-        $locationProvider.html5Mode(true);                      
+        // $locationProvider.html5Mode(true);  
+        // 关于angularjs-ui-router的 html5Model开启后的一些问题,解决的核心还是要配置nginx                    
     })
 
 
