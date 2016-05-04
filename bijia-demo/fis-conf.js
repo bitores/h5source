08bec8ee5fc21978aff8fe1fb2ac60a618e92f06
huangzhengjie <huangzhengjie@kuaiqiangche.com>
2016-05-04 13:18:26 +0800

    fis.match('*.html', {   //npm install -g fis-optimizer-html-minifier   optimizer: fis.plugin('html-minifier',{   	//option https://github.com/kangax/html-minifier   	minifyCSS: true,   	minifyJS: true,   	preserveLineBreaks:true   }) });

--- bijia-demo/fis-conf.js
+++ bijia-demo/fis-conf.js
@@ -145,9 +145,14 @@ fis.match('*.css', {
 // You need install it.
 // npm i fis-optimizer-html-minifier [-g]
 
-fis.match('*.html', {
-  //invoke fis-optimizer-html-minifier
-  optimizer: fis.plugin('html-minifier')
+fis.match('*.html', {  
+  //npm install -g fis-optimizer-html-minifier  
+  optimizer: fis.plugin('html-minifier',{
+    //option https://github.com/kangax/html-minifier
+    minifyCSS: true,
+    minifyJS: true,
+    preserveLineBreaks:true
+  })  
 });
 
 
